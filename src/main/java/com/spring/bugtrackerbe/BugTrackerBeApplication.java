81d61f139802d07edb4ea99160b3829df0877a3c
hoanglong236 <hahahalalala236@gmail.com>
2023-10-08 02:14:40 +0700

    Initial commit

---
+++ src/main/java/com/spring/bugtrackerbe/BugTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.bugtrackerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/bugtrackerbe/BugTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.bugtrackerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
